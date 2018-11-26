d5422ea41f9e3144e6885568cd35464bd81d7c34
Anil Bhattarai <anilbhattarai@gmail.com>
2018-11-26 18:56:43 -0500

    added events to hide menu when clicking links

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 /* Toggle Links for mobile screen */
-const toggleLink = document.querySelector('.toggle-link');
+const toggleLink = document.querySelector('#toggle-link');
 const navLinks = document.querySelectorAll('.nav-link');
-toggleLink.addEventListener('click', function (event) {
+const toggleMenu = function () {
     navLinks.forEach(function (link) {
         link.classList.toggle('show-link');
     })
-})
-
+};
+toggleLink.addEventListener('click', toggleMenu);
 /*  */
-
 /* create constants of main navigation links */
-
 const productLink = document.getElementById('product-link');
 const testimonialLink = document.getElementById('testimonial-link');
 const photoBoothLink = document.getElementById('photo-booth-link');
 const aboutLink = document.getElementById('about-link');
 
 /** create constants for the page sections */
-
 const productSection = document.getElementById('product-section');
 const testimonialSection = document.getElementById('testimonial-section');
 const photoBoothSection = document.getElementById('photo-booth-section');
@@ -30,24 +27,36 @@ aboutSection.style.display = 'none';
 /** add event listeners to the links to display particular section and hide others */
 
 productLink.addEventListener('click', function (event) {
+    navLinks.forEach(function (link) {
+        link.classList.remove('show-link');
+    })
     productSection.style.display = 'block';
     testimonialSection.style.display = 'none';
     photoBoothSection.style.display = 'none';
     aboutSection.style.display = 'none';
 })
 testimonialLink.addEventListener('click', function (event) {
+    navLinks.forEach(function (link) {
+        link.classList.remove('show-link');
+    })
     productSection.style.display = 'none';
     testimonialSection.style.display = 'block';
     photoBoothSection.style.display = 'none';
     aboutSection.style.display = 'none';
 })
 photoBoothLink.addEventListener('click', function (event) {
+    navLinks.forEach(function (link) {
+        link.classList.remove('show-link');
+    })
     productSection.style.display = 'none';
     testimonialSection.style.display = 'none';
     photoBoothSection.style.display = 'block';
     aboutSection.style.display = 'none';
 })
 aboutLink.addEventListener('click', function (event) {
+    navLinks.forEach(function (link) {
+        link.classList.remove('show-link');
+    })
     productSection.style.display = 'none';
     testimonialSection.style.display = 'none';
     photoBoothSection.style.display = 'none';
