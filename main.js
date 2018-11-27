ac4c630a646a37b8190d81da9de6c259da10939a
Anil Bhattarai <anilbhattarai@gmail.com>
2018-11-27 09:36:14 -0500

    added cross menu item to close the menu on mobile

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 /* Toggle Links for mobile screen */
-const toggleLink = document.querySelector('#toggle-link');
+const toggleLink = document.querySelector('.toggle-link');
+const crossLink = document.querySelector('.cross-link');
 const navLinks = document.querySelectorAll('.nav-link');
-const toggleMenu = function () {
+const toggleMenu = () => {
+    toggleLink.style.display = 'none';
+    crossLink.classList.add('show-link');
     navLinks.forEach(function (link) {
-        link.classList.toggle('show-link');
-    })
+        link.classList.add('show-link');
+    });
 };
+const crossMenu = () => {
+    toggleLink.style.display = 'initial';
+    crossLink.classList.remove('show-link');
+    navLinks.forEach(function (link) {
+        link.classList.remove('show-link');
+    });
+}
 toggleLink.addEventListener('click', toggleMenu);
+crossLink.addEventListener('click', crossMenu);
+
 /*  */
 /* create constants of main navigation links */
 const productLink = document.getElementById('product-link');
@@ -27,36 +39,36 @@ aboutSection.style.display = 'none';
 /** add event listeners to the links to display particular section and hide others */
 
 productLink.addEventListener('click', function (event) {
-    navLinks.forEach(function (link) {
-        link.classList.remove('show-link');
-    })
+    // navLinks.forEach(function (link) {
+    //     link.classList.remove('show-link');
+    // })
     productSection.style.display = 'block';
     testimonialSection.style.display = 'none';
     photoBoothSection.style.display = 'none';
     aboutSection.style.display = 'none';
 })
 testimonialLink.addEventListener('click', function (event) {
-    navLinks.forEach(function (link) {
-        link.classList.remove('show-link');
-    })
+    // navLinks.forEach(function (link) {
+    //     link.classList.remove('show-link');
+    // })
     productSection.style.display = 'none';
     testimonialSection.style.display = 'block';
     photoBoothSection.style.display = 'none';
     aboutSection.style.display = 'none';
 })
 photoBoothLink.addEventListener('click', function (event) {
-    navLinks.forEach(function (link) {
-        link.classList.remove('show-link');
-    })
+    // navLinks.forEach(function (link) {
+    //     link.classList.remove('show-link');
+    // })
     productSection.style.display = 'none';
     testimonialSection.style.display = 'none';
     photoBoothSection.style.display = 'block';
     aboutSection.style.display = 'none';
 })
 aboutLink.addEventListener('click', function (event) {
-    navLinks.forEach(function (link) {
-        link.classList.remove('show-link');
-    })
+    // navLinks.forEach(function (link) {
+    //     link.classList.remove('show-link');
+    // })
     productSection.style.display = 'none';
     testimonialSection.style.display = 'none';
     photoBoothSection.style.display = 'none';
